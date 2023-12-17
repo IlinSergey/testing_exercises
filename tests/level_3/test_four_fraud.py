c7a3fa948c546e7830d645594effa38d707a2819
Sergey Ilin <ilin_sergey90@mail.ru>
2023-12-17 16:32:41 +0300

    two with fabric

--- tests/level_3/test_four_fraud.py
+++ tests/level_3/test_four_fraud.py
@@ -2,29 +2,15 @@ import datetime
 from decimal import Decimal
 
 from functions.level_3.four_fraud import find_fraud_expenses
-from functions.level_3.models import (BankCard, Currency, Expense,
-                                      ExpenseCategory)
+from functions.level_3.models import Currency, ExpenseCategory
 
 
-def test__find_fraud_expenses__success(expenses_list):
-    expected_data = [
-        Expense(amount=Decimal('722.28'), currency=Currency.EUR,
-                card=BankCard(last_digits='1234', owner='John Doe'),
-                spent_in='leetcode.com', spent_at=datetime.datetime(2023, 12, 9, 17, 53, 28, 127368),
-                category=ExpenseCategory.TRANSPORT),
-        Expense(amount=Decimal('722.28'), currency=Currency.EUR,
-                card=BankCard(last_digits='1234', owner='John Doe'),
-                spent_in='leetcode.com', spent_at=datetime.datetime(2023, 12, 9, 17, 53, 28, 127368),
-                category=ExpenseCategory.TRANSPORT),
-        Expense(amount=Decimal('722.28'), currency=Currency.EUR,
-                card=BankCard(last_digits='1234', owner='John Doe'),
-                spent_in='leetcode.com', spent_at=datetime.datetime(2023, 12, 9, 17, 53, 28, 127368),
-                category=ExpenseCategory.TRANSPORT),
-        Expense(amount=Decimal('722.28'), currency=Currency.EUR,
-                card=BankCard(last_digits='1234', owner='John Doe'),
-                spent_in='leetcode.com', spent_at=datetime.datetime(2023, 12, 9, 17, 53, 28, 127368),
-                category=ExpenseCategory.TRANSPORT),
-    ]
+def test__find_fraud_expenses__success(expenses_list, expenses_list_fabric, get_card):
+    card = get_card
+    expected_data = expenses_list_fabric([(Decimal('722.28'), Currency.EUR,
+                                           card, 'leetcode.com', datetime.datetime(2023, 12, 9, 17, 53, 28, 127368),
+                                           ExpenseCategory.TRANSPORT) for _ in range(4)
+                                          ])
     assert find_fraud_expenses(expenses_list) == expected_data
 
 
--- tests/level_3/test_two_expense_categorizer.py
+++ tests/level_3/test_two_expense_categorizer.py
@@ -1,15 +1,25 @@
+import datetime
+from decimal import Decimal
+
 import pytest
 
-from functions.level_3.models import ExpenseCategory
+from functions.level_3.models import BankCard, Currency, ExpenseCategory
 from functions.level_3.two_expense_categorizer import guess_expense_category
 
 
-@pytest.mark.parametrize('index, expected_result', [
-    (0, ExpenseCategory.SUPERMARKET),
-    (1, ExpenseCategory.MEDICINE_PHARMACY),
-    (-1, ExpenseCategory.ONLINE_SUBSCRIPTIONS),
-    (5, ExpenseCategory.ONLINE_SUBSCRIPTIONS),
-    (-2, None),
+@pytest.mark.parametrize('arguments, expected_result', [
+    ([(Decimal('351.30'), Currency.RUB, BankCard(last_digits='1234', owner='John Doe'),
+       'chinar', datetime.datetime(2023, 12, 9, 17, 53, 28, 126551), ExpenseCategory.SUPERMARKET)],
+     ExpenseCategory.SUPERMARKET),
+    ([(Decimal('473.82'), Currency.EUR, BankCard(last_digits='1234', owner='John Doe'),
+       'alfa-pharm', datetime.datetime(2023, 12, 7, 17, 53, 28, 126620), ExpenseCategory.MEDICINE_PHARMACY),],
+     ExpenseCategory.MEDICINE_PHARMACY),
+    ([(Decimal('337.05'), Currency.RUB, BankCard(last_digits='1234', owner='John Doe'),
+       'zoom.us', datetime.datetime(2023, 12, 7, 17, 53, 28, 127785), ExpenseCategory.ONLINE_SUBSCRIPTIONS),],
+     ExpenseCategory.ONLINE_SUBSCRIPTIONS),
+    ([(Decimal('481.93'), Currency.AMD, BankCard(last_digits='1234', owner='John Doe'),
+       'not in category list', datetime.datetime(2023, 12, 8, 17, 53, 28, 127730), None),],
+     None),
 ])
-def test__guess_expense_category__succes(expenses_list, index, expected_result):
-    assert guess_expense_category(expenses_list[index]) == expected_result
+def test__guess_expense_category__succes(arguments, expenses_list_fabric, expected_result):
+    assert guess_expense_category(expenses_list_fabric(arguments)[0]) == expected_result
