b369df0264216d4eb40ef594fc4a8ffa1444729f
Sergey Ilin <ilin_sergey90@mail.ru>
2023-12-18 06:48:00 +0300

    fixture to conftest from level1 four

--- tests/level_1/conftest.py
+++ tests/level_1/conftest.py
@@ -1,8 +1,9 @@
 import datetime
+from decimal import Decimal
 
 import pytest
 
-from functions.level_1.four_bank_parser import BankCard, SmsMessage
+from functions.level_1.four_bank_parser import BankCard, Expense, SmsMessage
 
 
 @pytest.fixture
@@ -24,4 +25,22 @@ def sms_list() -> list[SmsMessage]:
                    author='Bank', sent_at=datetime.datetime.now()),
         SmsMessage(text='0 RUB, 445566775678 19.01.24 15:45 Wildberries authcode 1234',
                    author='Bank', sent_at=datetime.datetime.now()),
+    ]
+
+
+@pytest.fixture
+def get_result_expenses() -> list[Expense]:
+    return [
+        Expense(amount=Decimal('2500.00'),
+                card=BankCard(last_digits='1234', owner='John Doe'),
+                spent_in='Magnit',
+                spent_at=datetime.datetime(2023, 11, 18, 13, 31)),
+        Expense(amount=Decimal('1200.50'),
+                card=BankCard(last_digits='5678', owner='Jane Smith'),
+                spent_in='Walmart',
+                spent_at=datetime.datetime(2024, 1, 19, 15, 45)),
+        Expense(amount=Decimal('0'),
+                card=BankCard(last_digits='5678', owner='Jane Smith'),
+                spent_in='Wildberries',
+                spent_at=datetime.datetime(2024, 1, 19, 15, 45)),
     ]
--- tests/level_1/test_four_bank_parser.py
+++ tests/level_1/test_four_bank_parser.py
@@ -1,28 +1,6 @@
-import datetime
-from decimal import Decimal
-
 import pytest
 
-from functions.level_1.four_bank_parser import (BankCard, Expense,
-                                                parse_ineco_expense)
-
-
-@pytest.fixture
-def result_expenses() -> list[Expense]:
-    return [
-        Expense(amount=Decimal('2500.00'),
-                card=BankCard(last_digits='1234', owner='John Doe'),
-                spent_in='Magnit',
-                spent_at=datetime.datetime(2023, 11, 18, 13, 31)),
-        Expense(amount=Decimal('1200.50'),
-                card=BankCard(last_digits='5678', owner='Jane Smith'),
-                spent_in='Walmart',
-                spent_at=datetime.datetime(2024, 1, 19, 15, 45)),
-        Expense(amount=Decimal('0'),
-                card=BankCard(last_digits='5678', owner='Jane Smith'),
-                spent_in='Wildberries',
-                spent_at=datetime.datetime(2024, 1, 19, 15, 45)),
-    ]
+from functions.level_1.four_bank_parser import parse_ineco_expense
 
 
 @pytest.mark.parametrize('sms_index, expected_result_index', [
@@ -31,9 +9,9 @@ def result_expenses() -> list[Expense]:
     (3, 2),
 ])
 def test__parse_ineco_expense(sms_index, expected_result_index,
-                              bank_card_list, sms_list, result_expenses):
+                              bank_card_list, sms_list, get_result_expenses):
     assert parse_ineco_expense(sms_list[sms_index],
-                               bank_card_list) == result_expenses[expected_result_index]
+                               bank_card_list) == get_result_expenses[expected_result_index]
 
 
 @pytest.mark.parametrize('sms_index', [
